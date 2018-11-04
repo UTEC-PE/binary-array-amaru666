28f40f26880ce31be08af747a8939ef0134d3e78
jaox <jaox@jaoxs-MacBook-Air.local>
2018-11-04 17:25:23 -0500

    first commit

--- binary.cpp
+++ binary.cpp
@@ -1,34 +1,49 @@
 #ifndef BOOLEAN_H
 #define BOOLEAN_H
 
+#include <iostream>
+
 class boolean {
     private:
         char* array;
         int size;
     
     public:
-        boolean(int size) {
+        boolean(int size):size{size} {
             // TODO
+            this->array = new char[size/8+1];
         }
         
         void bitOn(int position) {
             // TODO
+            this->array[position/8] |= (1 <<(position %8));
         }
              
         void bitOff(int position) {
             // TODO
+            this->array[position/8] &= ~(1 << (position%8));
         }
              
         bool operator[](int position) {
             // TODO
+            return this->array[(int) position/8] & (1<<(position%8));
         }
     
         void clear() {    
             // TODO
+            for(int i = 0, tam = size / 8; i < tam; i++)
+                array[i] = 0;
+        }
+
+        void print() {
+            std::cout<<'\n';
+            for(int i = 0; i < size; i++)
+                std::cout<<(*this)[i];
         }
              
         ~boolean() {
             // TODO
+            delete [] array;
         }
 };
 
--- main.cpp
+++ main.cpp
@@ -1,13 +1,19 @@
 #include <iostream>
+#include <bitset>
+#include "binary.cpp"
 
 using namespace std;
 
 int main(int argc, char *argv[]) {
     cout << "===========================================================" << endl;
-    cout << "\Binary Array Practice" << endl;
+    cout << "Binary Array Practice" << endl;
     cout << "===========================================================" << endl << endl;
 
-    system("read");
+    boolean ss(20);
+    ss.print();
+    ss.bitOn(14);
+    ss.print();
+
+    //system("read");
     return EXIT_SUCCESS;
 }
-
